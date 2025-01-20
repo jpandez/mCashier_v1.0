b979d563665ac43494852d3fe82604ad07e1a174
rommel232 <rommel.oczon@tlcfzc.ae>
2025-01-20 17:06:00 +0800

    secret_data

---
+++ Controllers/private_data/CWT5bq4MmtAmTaHbvr3uHp.php
@@ -0,0 +1,8 @@
+<?php
+$_MAIL["user"] = base64_decode("dGVzdDJAd2ViYXBwbGljYXRpb25zZGV2ZWxvcGVyLm5ldA==");
+$_MAIL["password"]=base64_decode("dGVzdDEyMzQ=");
+$_MAIL["server"] = base64_decode("bWFpbC53ZWJhcHBsaWNhdGlvbnNkZXZlbG9wZXIubmV0");;
+$GLOBALS["MAIL"]=$_MAIL;
+$_GUISERVICE["user"] =base64_decode("dGVzdA==");
+$_GUISERVICE["password"]=base64_decode("dGVzdA==");
+?>
